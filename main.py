347a163da3b2fac206154aa0877fc7a649f2e83a
Robiiinos <dyrda.robin@outlook.fr>
2020-10-10 17:33:52 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import requests
+from operator import getitem
+
+data = requests.get(url = "https://launchpad.binance.com/gateway-api/v1/public/launchpool/project/list", headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+}).json()
+
+# Get the assets for which a Launchpool is currently in progress.
+assetsDict = dict()
+assetsSet = set()
+for project in data['data']['tracking']['list']:
+  assetsDict[project['projectId']] = dict()
+  assetsDict[project['projectId']]['projectId'] = project['projectId']
+  assetsDict[project['projectId']]['asset'] = project['asset']
+  assetsDict[project['projectId']]['yield'] = project['annualRate']
+  assetsSet.add(project['asset'])
+
+userAsset = input("Select your asset ({}): ".format(", ".join(sorted(assetsSet))))
+
+projectsDict = dict()
+for (key, value) in assetsDict.items():
+   if value['asset'] == userAsset:
+       projectsDict[value['projectId']] = value
+
+for (userProject, userProjectValue) in sorted(projectsDict.items(), key = lambda x: x[1]['yield'], reverse=True):
+  print('{} with {}% APY'.format(userProject, round(float(userProjectValue['yield']) * 100, 2)))
