77ecdf95d0b5bb75e6b49d5a778760b3e02dff87
Robiiinos <dyrda.robin@outlook.fr>
2020-10-10 23:37:13 +0200

    Add pycodestyle GitHub Actions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import requests
 
-data = requests.get(url = "https://launchpad.binance.com/gateway-api/v1/public/launchpool/project/list", headers = {
+data = requests.get(url="https://launchpad.binance.com/gateway-api/v1/public/launchpool/project/list", headers={
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
 }).json()
 
@@ -18,5 +18,5 @@ userAsset = input("Select your asset ({}): ".format(", ".join(sorted(assetsSet))
 
 # Filter the dict based on user input, sort it by highest yield, and print results.
 projectsDict = dict(filter(lambda elem: elem[1]['asset'] == userAsset, assetsDict.items()))
-for (userProject, userProjectValue) in sorted(projectsDict.items(), key = lambda x: x[1]['annualRate'], reverse=True):
+for (userProject, userProjectValue) in sorted(projectsDict.items(), key=lambda x: x[1]['annualRate'], reverse=True):
     print('{} with {}% APY'.format(userProject, round(float(userProjectValue['annualRate']) * 100, 2)))
