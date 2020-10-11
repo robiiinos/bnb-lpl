2c599722213743ced5a5e11a04eede4b1fed42b7
Robiiinos <dyrda.robin@outlook.fr>
2020-10-11 10:04:13 +0200

    Fix dict filtering

--- main.py
+++ main.py
@@ -4,19 +4,22 @@
 import requests
 
 data = requests.get(url="https://launchpad.binance.com/gateway-api/v1/public/launchpool/project/list", headers={
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
 }).json()
 
 # Get the assets for which a Launchpool is currently in progress.
 assetsDict = dict()
 assetsSet = set()
-for project in data['data']['tracking']['list']:
-    assetsDict[project['projectId']] = dict(project)
-    assetsSet.add(project['asset'])
+for project in data["data"]["tracking"]["list"]:
+    assetsDict[project["projectId"]] = dict(project)
+    assetsSet.add(project["asset"])
 
 userAsset = input("Select your asset ({}): ".format(", ".join(sorted(assetsSet))))
 
 # Filter the dict based on user input, sort it by highest yield, and print results.
-projectsDict = dict(filter(lambda elem: elem[1]['asset'] == userAsset, assetsDict.items()))
-for (userProject, userProjectValue) in sorted(projectsDict.items(), key=lambda x: x[1]['annualRate'], reverse=True):
-    print('{} with {}% APY'.format(userProject, round(float(userProjectValue['annualRate']) * 100, 2)))
+projectsDict = sorted(dict(filter(lambda elem: elem[1]["asset"] == userAsset, assetsDict.items())).items(), key=lambda x: x[1]["annualRate"], reverse=True)
+
+for (userProject, userProjectValue) in projectsDict:
+    name = userProject.split("_")[0]
+    apy = round(float(userProjectValue["annualRate"]) * 100, 2)
+    print("{} with {}% APY".format(name, apy))
