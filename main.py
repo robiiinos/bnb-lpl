5f241c0033820db192b8092c7caf0c33308698ac
Robiiinos <dyrda.robin@outlook.fr>
2020-10-11 10:49:02 +0200

    Fix #1

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ for project in data["data"]["tracking"]["list"]:
     assetsDict[project["projectId"]] = dict(project)
     assetsSet.add(project["asset"])
 
-assetInput = input("Select your asset ({}): ".format(", ".join(sorted(assetsSet)))) or "BNB"
+assetInput = ""
+while assetInput.upper() not in assetsSet:
+    assetInput = input("Select your asset ({}): ".format(", ".join(sorted(assetsSet)))) or "BNB"
 
-# Filter the dict based on user input, sort it by highest yield, and print results.
-projectsDict = sorted(assetsDict.items(), key=lambda x: x[1]["annualRate"], reverse=True)
-for index, (projectKey, projectValue) in enumerate(projectsDict):
-    if projectValue["asset"] == assetInput.upper():
-        name = projectKey.split("_")[0]
-        apy = round(float(projectValue["annualRate"]) * 100, 2)
-        print("{}. {} with {}% APY".format(index + 1, name, apy))
+    # Filter the dict based on user input, sort it by highest yield, and print results.
+    projectsDict = sorted(assetsDict.items(), key=lambda x: x[1]["annualRate"], reverse=True)
+    for index, (projectKey, projectValue) in enumerate(projectsDict):
+        if projectValue["asset"] == assetInput.upper():
+            name = projectKey.split("_")[0]
+            apy = round(float(projectValue["annualRate"]) * 100, 2)
+            print("{}. {} with {}% APY".format(index + 1, name, apy))
