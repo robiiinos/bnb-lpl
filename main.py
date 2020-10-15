b0f104a379900f0628813147cc7ec8d02026f31e
Robiiinos <dyrda.robin@outlook.fr>
2020-10-15 20:34:23 +0200

    pythonic way scripting

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,line-too-long
 
 import requests
 import shadow_useragent
 
-data = requests.get(url="https://launchpad.binance.com/gateway-api/v1/public/launchpool/project/list", headers={
-    "Accept": "application/json",
-    "User-Agent": shadow_useragent.ShadowUserAgent().most_common
-}).json()
+URL = "https://launchpad.binance.com/gateway-api/v1/public/launchpool/project/list"  # nopep8
+data = requests.get(
+    url=URL,
+    headers={
+        "Accept": "application/json",
+        "User-Agent": shadow_useragent.ShadowUserAgent().most_common
+    }
+).json()
 
 # Get the assets for which a Launchpool is currently in progress.
 assetsDict = dict()
@@ -18,14 +21,23 @@ for project in data["data"]["tracking"]["list"]:
 
 ASSET_INPUT = ""
 while ASSET_INPUT.upper() not in assetsSet:
-    ASSET_INPUT = input("Select your asset ({}): ".format(", ".join(sorted(assetsSet)))) or "BNB"
+    # Fetch user input for asset, and set default if none.
+    ASSET_INPUT = input("Select your asset ({}): ".format(
+            ", ".join(sorted(assetsSet))
+        )
+    ) or "BNB"
 
-    # Filter the dict based on user input, sort it by highest yield, and print results.
+    # 1. Filter the dict based on user input
+    # 2. Sort the dict by highest yield
     projectsDict = filter(
         lambda elem: elem[1]['asset'] == ASSET_INPUT,
-        sorted(assetsDict.items(), key=lambda x: x[1]["annualRate"], reverse=True)
+        sorted(
+            assetsDict.items(),
+            key=lambda x: x[1]["annualRate"],
+            reverse=True)
     )
 
+    # Print results.
     for index, key in enumerate(projectsDict):
         name = key[0].split("_")[0]
         apy = round(float(key[1]["annualRate"]) * 100, 2)
